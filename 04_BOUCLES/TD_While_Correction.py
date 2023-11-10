9c0dfd05b64c1c4b6f715ab2f1f91311bcdeb691
EnzoFmx <enzofremeaux@gmail.com>
2023-11-10 11:08:21 +0100

    ajout auto script

--- 04_BOUCLES/TD_While_Correction.py
+++ 04_BOUCLES/TD_While_Correction.py
@@ -74,4 +74,81 @@ def voyelle(chaine):
         elif chaine[l] =='y' :
             chaine_finale = chaine_finale + chaine[l]
         l = l+1
-    return chaine_finale  
+    return chaine_finale
+
+
+# COMPTE A REBOURS :
+
+import time
+
+def compte_a_rebours(temps):
+    while temps>0 :
+        print(temps)
+        temps = temps-1
+        # Permet de faire une pause d'une seconde
+        time.sleep(1)
+    return 'Fin'
+
+
+
+
+# PIERRE FEUILLE CISEAUX :
+
+
+"""
+Raisonnement approximatif :
+
+tant qu'un des deux joueur n'a pas 3 points
+
+    joueur 1 choisi entre pierre feuille ou ciseaux
+    joueur 2 choisi entre pierre feuille ou ciseaux
+
+    comparaison des deux réponses :
+        selon les choix =>
+            soit j1 gagne 1 point
+            
+            soit j2 gagne 1 point
+            
+            soit personne ne gagne de point
+
+"""
+
+
+
+def pfc():
+    resultat_J1 = 0
+    resultat_J2 = 0
+
+    while resultat_J1  != 3 and resultat_J2 != 3 :
+        choix_J1 = input("Joueur 1 choisis entre pierre feuille et ciseaux")
+        choix_J2 = input("Joueur 2 choisis entre pierre feuille et ciseaux")
+        
+        if choix_J1 == "pierre" and choix_J2 == "ciseaux" :
+            print("J1 gagne cette manche")
+            resultat_J1 += 1 #resultat_J1 = resultat_J1 + 1
+        elif choix_J1 == "feuille" and choix_J2 == "pierre" :
+            print("J1 gagne cette manche")
+            resultat_J1 += 1 #resultat_J1 = resultat_J1 + 1
+        elif choix_J1 == "ciseaux" and choix_J2 == "feuille" :
+            print("J1 gagne cette manche")
+            resultat_J1 += 1 #resultat_J1 = resultat_J1 + 1
+        elif choix_J1 == "feuille" and choix_J2 == "ciseaux" :
+            print("J2 gagne cette manche")
+            resultat_J2 += 1 #resultat_J1 = resultat_J1 + 1
+        elif choix_J1 == "ciseaux" and choix_J2 == "pierre" :
+            print("J2 gagne cette manche")
+            resultat_J2 += 1 #resultat_J1 = resultat_J1 + 1
+        elif choix_J1 == "pierre" and choix_J2 == "feuille" :
+            print("J2 gagne cette manche")
+            resultat_J2 += 1 #resultat_J1 = resultat_J1 + 1
+        else :
+            print('Egalité')
+
+    if resultat_J1 == 3 :
+        print('GG J1')
+    else :
+        print('GG J2')
+
+
+
+
